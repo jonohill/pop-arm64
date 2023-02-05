186ad0b0fb3630cd12b5ef9798ea7b5d8fe7df0e
Jono Hill <jono@hillnz.com>
2023-02-05 22:26:15 +1300

    sudo

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-apt-get update
-apt-get install -y debootstrap mtools parted gnupg systemd-container eatmydata rsync git squashfs-tools
+sudo apt-get update
+sudo apt-get install -y debootstrap mtools parted gnupg systemd-container eatmydata rsync git squashfs-tools
 
 git clone https://github.com/pop-os/pop-arm64.git pop
 cd pop
